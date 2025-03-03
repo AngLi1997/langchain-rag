7191d83603cf76e0822bee78fbd13c0bfbc728d4
liang <liangliangaichirou@gmail.com>
2025-03-04 01:29:29 +0800

    thinking

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import gradio
-from gradio.components.button import Button
 from gradio.components.clear_button import ClearButton
 from gradio.components.textbox import Textbox
 from langchain_ollama import ChatOllama
@@ -10,16 +9,21 @@ if __name__ == '__main__':
     with gradio.Blocks() as demo:
         chatbot = gradio.Chatbot(type="messages")
         input_box = Textbox(label="请输入问题", placeholder="Enter text and press enter")
-        button = Button("发送")
         def chat(message, history):
-            history.append({"role": "user","content":message})
-            resp = {"role": "assistant", "content":""}
-            history.append(resp)
-            print(history)
-            for chunk in llm.stream(input=history):
-                resp["content"] += chunk.content
+            history.append({"role": "user", "content": message})
+            chunks = llm.stream(input=history)
+            resp = {}
+            for chunk in chunks:
+                print(chunk.content)
+                if chunk.content == "<think>":
+                    resp = {"role": "assistant", "content": "", "metadata": {"title":"thinking"}}
+                    history.append(resp)
+                elif chunk.content == "</think>":
+                    resp = {"role": "assistant", "content": ""}
+                    history.append(resp)
+                else:
+                    resp["content"] += chunk.content
                 yield "", history
         input_box.submit(chat, [input_box, chatbot], [input_box, chatbot])
-        button.click(chat, [input_box, chatbot], [input_box, chatbot])
         clear = ClearButton([chatbot, input_box])
     demo.launch()
