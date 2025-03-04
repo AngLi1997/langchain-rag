467d8a0a00ccac0cee986356a5c0c22ed0be79e0
李昂 <liangliangaichirou@163.com>
2025-03-04 18:40:02 +0800

    main

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+import os
+
 import gradio
 from gradio import Textbox
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain_core.tools import Tool
 from langchain_ollama import ChatOllama
 from langgraph.constants import START, END
 from langgraph.graph import add_messages, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
 from typing_extensions import TypedDict, Annotated
 
-llm = ChatOllama(model="deepseek-r1:32b", base_url="http://192.168.110.129:11434")
+os.environ["LANGSMITH_TRACING"] = "true"
+os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGSMITH_PROJECT"] = "liang-lang-smith"
+os.environ["LANGSMITH_API_KEY"] = "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"
+os.environ["SERPER_API_KEY"] = "165d4285a70a571a0659b3bda8cd61da7c273f18"
 
+llm = ChatOllama(model="llama3.1:latest", base_url="http://192.168.110.129:11434")
+search = GoogleSerperAPIWrapper()
+tools = [Tool(name="google search", func=search.results, description="google search")]
+llm_with_tools = llm.bind_tools(tools)
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
@@ -14,7 +27,7 @@ class State(TypedDict):
 
 def chatbot(state: State):
     res = ""
-    for chunk in llm.stream(state["messages"]):
+    for chunk in llm_with_tools.stream(state["messages"]):
         res += chunk.content
         yield {"messages": res}
 
@@ -22,6 +35,10 @@ def chatbot(state: State):
 graph_builder = StateGraph(State)
 
 graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", ToolNode(tools=tools))
+graph_builder.add_conditional_edges("chatbot", tools_condition)
+graph_builder.add_edge("tools", "chatbot")
+
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 
@@ -29,27 +46,26 @@ graph = graph_builder.compile()
 
 print(graph.get_graph().draw_ascii())
 
-# for msg, event in graph.stream({"messages": [{"role": "user", "content": "1+1在什么情况下等于3"}]}, stream_mode="messages"):
-#     print(msg.content, end="")
-
 
 if __name__ == '__main__':
-    with gradio.Blocks() as demo:
+    with gradio.Blocks(title="langgraph-deepseek") as demo:
         chatbot = gradio.Chatbot(type="messages")
         input_box = Textbox(label="请输入问题", placeholder="Enter text and press enter")
         def chat(message, history):
             history.append({"role": "user", "content": message})
             chunks = graph.stream({"messages": history}, stream_mode="messages")
-            resp = {}
+            # resp = {}
+            resp = {"role": "assistant", "content": ""}
+            history.append(resp)
             for chunk, event in chunks:
-                if chunk.content == "<think>":
-                    resp = {"role": "assistant", "content": "", "metadata": {"title":"thinking"}}
-                    history.append(resp)
-                elif chunk.content == "</think>":
-                    resp = {"role": "assistant", "content": ""}
-                    history.append(resp)
-                else:
-                    resp["content"] += chunk.content
+                # if chunk.content == "<think>":
+                #     resp = {"role": "assistant", "content": "", "metadata": {"title":"thinking"}}
+                #     history.append(resp)
+                # elif chunk.content == "</think>":
+                #     resp = {"role": "assistant", "content": ""}
+                #     history.append(resp)
+                # else:
+                resp["content"] += chunk.content
                 yield "", history
         input_box.submit(chat, [input_box, chatbot], [input_box, chatbot])
     demo.launch()
